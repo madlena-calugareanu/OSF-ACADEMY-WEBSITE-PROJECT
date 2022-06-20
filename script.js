46bfc4e52e2bbf4a03adfeff9ad597fe0ed1e9fd
Madlena C <madlena.calugareanu@gmail.com>
2022-06-20 21:10:07 +0300

    updated html, css and js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 $(document).ready(()=>{
 
     if(window.matchMedia('(min-width: 1280px)').matches){
+        // Hover on mega-menu//
         $('.mega-box').hide();
         $('#services').on('mouseenter',()=>{
             $('.mega-box').show();
@@ -11,18 +12,60 @@ $(document).ready(()=>{
             $('#services').removeClass('services-toggle')
         });
     }
-    // Hover on mega-menu//
     
-
     // Display menu on click //   
-    if(window.matchMedia('(max-width: 1280px)').matches)     
-    $('.fa-solid').on('click',()=>{
-        $('.nav-links').toggle().addClass('mobile-menu');
-        $('.fa-x').toggle();
-    });
-    $('#services').on('click',()=>{
-        $('.mega-box').toggle().addClass('mobile-mega');
-    });
+    if(window.matchMedia('(max-width: 1280px)').matches){
+        $('.fa-bars').on('click',()=>{
+            $('.nav-links').toggle().addClass('mobile-menu');
+            $('.fa-x').show();
+        });
+        $('.fa-x').on('click', ()=>{
+            $('.nav-links').toggle().removeClass('mobile-menu');
+            $('.fa-x').hide();
+        })
+        $('#services').on('click',()=>{
+            $('.mega-box').toggle().addClass('mobile-mega');
+            $('#services-down').toggle();
+            $('#services-up').toggle();
+        });
+        $('.mega-links').hide();
+        $('#product-name').on('click', ()=>{
+            $('#product-col').toggle();  
+            $('#product-down').toggle();
+            $('#product-up').toggle();         
+        });
+        $('#sale-name').on('click', ()=>{
+            $('#sale-col').toggle();   
+            $('#sale-down').toggle();
+            $('#sale-up').toggle();  
+        });
+    }
 
-
+    if(window.matchMedia('(max-width: 768px)').matches){
+            $('.fa-bars').on('click',()=>{
+            $('.nav-links').show().addClass('mobile-menu');
+            $('.fa-bars').hide();
+            $('.fa-x').show();
+        });
+        $('.fa-x').on('click', ()=>{
+            $('.nav-links').hide().removeClass('mobile-menu');
+            $('.fa-bars').show();
+            $('.fa-x').hide();
+        });
+        $('#contact').on('click', ()=>{
+            $('.contact-links').toggle();
+            $('#contact-down').toggle();
+            $('#contact-up').toggle();
+        });
+        $('#categories').on('click', ()=>{
+            $('.categories-links').toggle();
+            $('#categories-down').toggle();
+            $('#categories-up').toggle();
+        });
+        $('#about').on('click', ()=>{
+            $('.about-links').toggle();
+            $('#about-down').toggle();
+            $('#about-up').toggle();
+        })
+    }
 });
