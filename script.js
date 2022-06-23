c2cfce86b03d1fb884372d5247b5f1b20b2372d9
Madlena C <madlena.calugareanu@gmail.com>
2022-06-23 14:29:26 +0300

    add popular items columns and load more

--- script.js
+++ script.js
@@ -68,4 +68,21 @@ $(document).ready(()=>{
             $('#about-up').toggle();
         })
     }
+
+
+
 });
+
+$( document ).ready(function () {
+    $(".moreBox").slice(0, 4).show();
+      if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+      }   
+      $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 8).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+          $("#loadMore").fadeOut('slow');
+        }
+      });
+    });
